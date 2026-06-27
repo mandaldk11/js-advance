b23bc235dd1b28440dc6665d0678b6b4a9b09a6b
Dharmendra mandal <mandaldk11@gmail.com>
2026-06-27 22:15:22 +0530

    data types

---
+++ Basics/variables.js
@@ -0,0 +1,23 @@
+//1)Declairation of variables
+
+const accountId=1234;
+// accountId=123; not allowed
+let name="dk"
+name="Dharmendra"
+var age=26
+age=27
+
+email="dk@gmail.com"
+email="dm@gmail.com"
+
+//note- not use var as block and functional scope
+
+// console.log(accountId);
+console.table([accountId,name,age,email])
+
+// 2) Data Types-[string,num,boolean,BigInt,Symbol,undefined,null,object,]
+// alert("hell0") --not possible bz we are using node js not browser
+// console.log("hi") console.log('gg') ---need semicolon to write in new line
+//***/ typeOf(null)=object
+// typeOf(undefined)=undefined
+// number - 2 power 53
